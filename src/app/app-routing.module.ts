d767404d6b06052f3ea7c0ec1ae92808e9400360
Michaela <michaela_rodenbuecher@web.de>
2019-01-24 12:05:11 +0100

    Navigation angelegt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { EnvironmentComponent } from './environment/environment.component';
+import { FirstAppComponent } from './first-app/first-app.component';
+import { TypescriptComponent } from './typescript/typescript.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'environment', component: EnvironmentComponent },
+  { path: 'first-app', component: FirstAppComponent },
+  { path: 'typescript', component: TypescriptComponent },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { EnvironmentComponent } from './environment/environment.component';
+import { FirstAppComponent } from './first-app/first-app.component';
+import { TypescriptComponent } from './typescript/typescript.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EnvironmentComponent,
+    FirstAppComponent,
+    TypescriptComponent
   ],
   imports: [
     BrowserModule,
