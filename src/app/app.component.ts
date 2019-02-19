1e02cc5c95af2a8eb27ba128e669271705c7b410
Michaela <michaela_rodenbuecher@web.de>
2019-02-19 12:25:22 +0100

    Vor und Zurück Buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
-import { EnvironmentComponent } from './environment/environment.component';
+import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,44 @@ import { EnvironmentComponent } from './environment/environment.component';
 export class AppComponent {
 
   title = 'Master-Project';
+
+
+
+  public routerLinks = ["/home", "/environment", "/first-app", "/typescript", "/project-overview",
+    "/application", "/components", "/components-formmodules-directives", "/reusable-components",
+    "/services", "/routing", "/http", "/finish-app"];
+  public routerLink: string;
+
+  constructor(private router: Router, public location: Location) { }
+
+  next(): void {
+    this.routerLink = this.router.url;
+    let counter = -1;
+    for (let r of this.routerLinks) {
+      counter++;
+      if (this.routerLink === r) {
+        this.routerLink = this.routerLinks[counter + 1];
+        break;
+      }
+    }
+    console.log(this.routerLink);
+    this.location.go(this.routerLink);
+    location.reload;
+
+  }
+
+  back(): void {
+    this.routerLink = this.router.url;
+    let counter = -1;
+    for (let r of this.routerLinks) {
+      counter++;
+      if (this.routerLink === r) {
+        this.routerLink = this.routerLinks[counter - 1];
+        break;
+      }
+    }
+    console.log(this.routerLink);
+    this.location.go(this.routerLink);
+  }
+
 }
