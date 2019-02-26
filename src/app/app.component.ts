c545f971beb6b3e65921220cdbd17848d8732799
Michaela <michaela_rodenbuecher@web.de>
2019-02-26 15:06:46 +0100

    Vorbereitung für Mini-Modi-Navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
-import { Router, Route } from '@angular/router';
+import { Router } from '@angular/router';
+import { Modus } from './shared/modus';
+import { ModiService } from './shared/modi.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styles: []
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   title = 'Master-Project';
+  modus: Modus;
+  modi: Modus[];
 
   public routerLinks = ["/home", "/environment", "/first-app", "/typescript", "/project-overview",
     "/application", "/components", "/components-formmodules-directives", "/reusable-components",
     "/services", "/routing", "/http", "/finish-app"];
   public routerLink: string;
 
-  constructor(private router: Router, public location: Location) { }
+  constructor(private router: Router, public location: Location, private service: ModiService) { }
+
+  ngOnInit() {
+    this.getModi();
+  }
+
+  getModi(): void{
+    this.service.getModi()
+    .subscribe(modi => this.modi = modi);
+  }
+
+
+  theorist(): void {
+    this.service.setModus(this.modi[0])
+    .subscribe(modus => this.modus = modus);
+    console.log(this.modus);
+  }
+
+  practitioner(): void {
+    this.service.setModus(this.modi[1])
+    .subscribe(modus => this.modus = modus);
+    console.log(this.modus);
+  }
+
+  both(): void {
+    this.service.setModus(this.modi[2])
+    .subscribe(modus => this.modus = modus);
+    console.log(this.modus);
+  }
 
   getNext(): string {
     this.routerLink = this.router.url;
