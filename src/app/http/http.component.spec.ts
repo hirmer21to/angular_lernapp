777d4269800eee38ef9ae032ee18939cc5d4e745
lema <leon_martin@t-online.de>
2021-07-02 11:13:32 +0200

    Update angular (#14)
    * update angular
    
    * update angular
    
    * update angular
    
    * update angular
    
    * update angular
    
    * update angular
    
    * update angular
    
    * update angular final
    
    Co-authored-by: Tobias Hirmer <tobias.hirmer@uni-bamberg.de>

--- src/app/http/http.component.spec.ts
+++ src/app/http/http.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { HttpComponent } from './http.component';
 
@@ -6,7 +6,7 @@ describe('HttpComponent', () => {
   let component: HttpComponent;
   let fixture: ComponentFixture<HttpComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HttpComponent ]
     })
